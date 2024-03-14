e279d8390d62166afb6020cc38f7a9803ba74ab0
Bryce Kahle <bryce.kahle@datadoghq.com>
2024-03-14 16:20:36 -0700

    use common process function for rhel

--- pkg/repo/rhel.go
+++ pkg/repo/rhel.go
@@ -2,15 +2,12 @@ package repo
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"log"
 	"sort"
 
 	"github.com/aquasecurity/btfhub/pkg/job"
 	"github.com/aquasecurity/btfhub/pkg/kernel"
 	"github.com/aquasecurity/btfhub/pkg/pkg"
-	"github.com/aquasecurity/btfhub/pkg/utils"
 )
 
 type RHELRepo struct {
@@ -47,16 +44,5 @@ func (d *RHELRepo) GetKernelPackages(
 	}
 	sort.Sort(pkg.ByVersion(pkgs))
 
-	for _, p := range pkgs {
-		err := processPackage(ctx, p, workDir, opts, jobChan)
-		if err != nil {
-			if errors.Is(err, utils.ErrKernelHasBTF) {
-				log.Printf("INFO: kernel %s has BTF already, skipping later kernels\n", p)
-				return nil
-			}
-			return err
-		}
-	}
-
-	return nil
+	return processPackages(ctx, workDir, pkgs, opts, jobChan)
 }
