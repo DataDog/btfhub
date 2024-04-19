b7d1b883ea0c5541cc288f1164a77dfebe0c7dd8
Bryce Kahle <bryce.kahle@datadoghq.com>
2024-04-19 10:37:59 -0700

    retry connection refused

--- pkg/repo/debian.go
+++ pkg/repo/debian.go
@@ -14,6 +14,7 @@ import (
 	"slices"
 	"sort"
 	"strings"
+	"syscall"
 
 	"github.com/DataDog/btfhub/pkg/kernel"
 	"github.com/DataDog/btfhub/pkg/pkg"
@@ -236,6 +237,9 @@ func queryJsonAPI[T any](ctx context.Context, url string, out *T, headers map[st
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
+		if errors.Is(err, syscall.ECONNRESET) {
+			return fmt.Errorf("%w: %s", errRetry, err)
+		}
 		return err
 	}
 	defer resp.Body.Close()
