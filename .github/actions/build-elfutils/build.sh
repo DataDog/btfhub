10fbb75e23a6ac92fbcb65300043b6de8a5462d6
Bryce Kahle <bryce.kahle@datadoghq.com>
2025-01-08 14:53:43 -0800

    copy script into container

--- .github/actions/build-elfutils/build.sh
+++ .github/actions/build-elfutils/build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
-NPROC=$(nproc)
-
 wget -nv "https://sourceware.org/elfutils/ftp/${VERSION}/elfutils-${VERSION}.tar.bz2"
 tar xf "elfutils-${VERSION}.tar.bz2"
 cd "elfutils-${VERSION}"
 ./configure --disable-libdebuginfod --disable-debuginfod --disable-demangler
-make -j "${NPROC}" install
+make -j "$(nproc)" install
 ldconfig
