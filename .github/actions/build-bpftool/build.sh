d6cab8ca8133fd50f447057480cb7e5d497d37d9
Bryce Kahle <bryce.kahle@datadoghq.com>
2025-01-08 16:39:24 -0800

    sudo only for runner

--- .github/actions/build-bpftool/build.sh
+++ .github/actions/build-bpftool/build.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 cd ./3rdparty/bpftool
-sudo -E PATH="$PATH" make -j "$(nproc)" -C src/ V=1 install-bin
+${SUDO} make -j "$(nproc)" -C src/ V=1 install-bin
--- .github/actions/build-pahole/build.sh
+++ .github/actions/build-pahole/build.sh
@@ -5,6 +5,6 @@ cd ./3rdparty/dwarves
 mkdir build
 cd build
 cmake -D__LIB=lib -DDWARF_INCLUDE_DIR=/usr/include ..
-sudo make -j "$(nproc)" install
+${SUDO} make -j "$(nproc)" install
 echo "/usr/local/lib" >> /etc/ld.so.conf.d/pahole.conf
-sudo ldconfig
+${SUDO} ldconfig
