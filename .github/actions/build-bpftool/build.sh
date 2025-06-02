43caced832b05064578db79d56210410e3ee8abe
Bryce Kahle <bryce.kahle@datadoghq.com>
2025-06-02 11:56:34 -0700

    remove github actions for BTF

--- .github/actions/build-bpftool/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -exo pipefail
-
-cd ./3rdparty/bpftool
-${SUDO} make -j "$(nproc)" -C src/ V=1 install-bin
--- .github/actions/build-tar/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-wget -nv "https://ftpmirror.gnu.org/tar/tar-${VERSION}.tar.xz"
-tar xf "tar-${VERSION}.tar.xz"
-pushd "tar-${VERSION}"
-FORCE_UNSAFE_CONFIGURE=1 ./configure --prefix="${PREFIX}"
-make -j "$(nproc)" install
