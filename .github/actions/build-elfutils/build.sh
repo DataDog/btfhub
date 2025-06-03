950d86b3023a1d5996d1eaf3aac8ea3bbb21f406
Bryce Kahle <bryce.kahle@datadoghq.com>
2025-06-03 11:21:46 -0700

    remove github actions for BTF (#52)

--- .github/actions/build-elfutils/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-wget -nv "https://sourceware.org/elfutils/ftp/${VERSION}/elfutils-${VERSION}.tar.bz2"
-tar xf "elfutils-${VERSION}.tar.bz2"
-cd "elfutils-${VERSION}"
-./configure --disable-libdebuginfod --disable-debuginfod --disable-demangler
-make -j "$(nproc)" install
-ldconfig
